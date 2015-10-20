c45e290259345a73724be5078155b0cdb39ef543
flinder <fridolin.linder@gmail.com>
2015-10-20 09:55:39 -0400

    random sleep in google scraper

--- data_collection/ncsl_tables/scrape_tables.py
+++ data_collection/ncsl_tables/scrape_tables.py
@@ -6,15 +6,18 @@ from random import randint
 
 outfile = io.open('ncsl_table_urls.txt', 'w+', encoding='utf-8')
 gs = GoogleSearch('site:ncsl.org "laws.aspx"')
-gs.results_per_page = 25 
+gs.results_per_page = 4 
 results = []
 
 tmp = 1
+i = 1
 while tmp is not None:
-    sleep(randint(0,100))
+    print('Page: {}'.format(i))
     tmp = gs.get_results()
     results.extend(tmp)
-
+    sleep(randint(0,100))
+    i += 1
+ 
 outfile.write('title,description,url')
 for res in results:
     row = '{},{},{}\n'.format(res.title, res.desc, res.url)

dad2b6ffd37b8e776c08138472df9711ba789a9f
flinder <fridolin.linder@gmail.com>
2015-12-11 18:20:59 -0500

    updated paper with new qap results

--- manuscript/make_diff.sh
+++ manuscript/make_diff.sh
@@ -1,12 +1,18 @@
 # Generate and compile latex diff to verion of the manuscript in
 # a commit. Commit SHA (id) to compare to is taken as commandline argument
 
+# Get the old version from the relevant commit 
 git checkout $1 text_reuse.tex
+# Store it in temporary file
 cp text_reuse.tex temp
+# Restore the most recent version under the original filename
 git checkout HEAD text_reuse.tex
+# Make the latex diff
 latexdiff temp text_reuse.tex > weekly_diff.tex
+# Compile latex
 pdflatex weekly_diff.tex
 bibtex weekly_diff
 pdflatex weekly_diff.tex
 pdflatex weekly_diff.tex
+# Remove temporary file
 rm temp

4f67a98921c7795d05bd65faa45f165fc8c1a7d5
flinder <fridolin.linder@gmail.com>
2016-01-20 14:20:17 -0500

    weekly diff

--- etl/ncsl_tables/get_table_urls.py
+++ etl/ncsl_tables/get_table_urls.py
@@ -8,7 +8,7 @@ import re
 
 def bing_request(offset, keyBing):
     credentialBing = 'Basic ' + (':%s' % keyBing).encode('base64')[:-1] # the "-1" is to remove the trailing "\n" which encode adds
-    searchString = '%27site%3Ancsl.org%20%22legislation.aspx%22%27'
+    searchString = '%27site%3Ancsl.org%20%22laws.aspx%22%27'
     top = 50
     url = 'https://api.datamarket.azure.com/Bing/Search/Web?' + \
           'Query=%s&$top=%d&$skip=%d&$format=json' % (searchString, top, offset)
@@ -19,7 +19,7 @@ def bing_request(offset, keyBing):
     response = json.load(response)
     return response
 
-outfile = io.open('../../data/ncsl/scraped_table_urls.csv', 'w+', encoding='utf-8')
+outfile = io.open('../../data/ncsl/scraped_table_urls_laws.csv', 'w+', encoding='utf-8')
 outfile.write('id,title,url,description,uri\n')
 offset = 0
 punct = re.compile(r'[,;\t|]')
@@ -38,7 +38,7 @@ while True:
         description = punct.sub('', result['Description'])
         
         # Write row to file
-        row = '{},{},{},{},{}\n'.format(id_,title,url_,description,uri)
+        row = '{},{},{},{}\n'.format(id_,title,url_,description)
         outfile.write(row)
 
     if '__next' not in response['d'].keys():

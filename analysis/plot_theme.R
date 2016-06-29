ce7a1a359588c0be2eb8594388e028869a9addfa
flinder <fridolin.linder@gmail.com>
2016-06-29 15:12:12 +0200

    updated plot theme

--- analysis/plot_theme.R
+++ analysis/plot_theme.R
@@ -4,6 +4,7 @@ cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2",
                "#D55E00", "#CC79A7")
 p_width <- 18.9
 
-plot_theme <- theme(axis.text=element_text(size=12),
-                    axis.title=element_text(size=14), legend.text=element_text(size=12)) +
-    theme_bw()
+plot_theme <- theme_bw() + 
+    theme(axis.text=element_text(size=12),
+          axis.title=element_text(size=18), 
+          legend.text=element_text(size=12))

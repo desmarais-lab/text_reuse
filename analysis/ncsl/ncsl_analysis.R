4a36b02b8f3d599f614368a49deec1f4f26963d3
flinder <fridolin.linder@gmail.com>
2016-07-20 16:01:42 +0200

    Refactored data preprocessing for anlyses
    - preprocessing for ideology and ncsl analysis is now done in one script:
    - `/etl/make_analysis_datasets.R`
    - This script also produces descriptive statistics for the general alignment
      dataset `/etl/alignemtn_descriptives.txt` the ideology dataset
      `/analysis/ideology/??.txt` and the ncsl dataset `/analysis/ncsl/??.txt`
    - Resultfiles added to gitignore

--- analysis/ncsl/ncsl_analysis.R
+++ analysis/ncsl/ncsl_analysis.R
@@ -2,92 +2,13 @@ library(dplyr)
 library(ggplot2)
 library(xtable)
 
-# Load and preprocess data
-# ==============================================================================
-
-# Load ncsl_analysis.RData to skip processing (takes a long time)
-
-alignments <- tbl_df(read.csv('../../data/lid/alignments_1000_b2b_ns.csv',
-                              stringsAsFactors = FALSE, header = TRUE))
-
-
-
-
-ncsl_bills <- tbl_df(read.csv('../../data/ncsl/ncsl_data_from_sample_matched.csv',
-                              stringsAsFactors = FALSE, header = TRUE)) %>% 
-    filter(!is.na(matched_from_db)) %>%
-    select(-description, -table) 
-
-
-# Data Frame of all pairs we have table information of (with the matched db ids)
-bill_pairs <- tbl_df(as.data.frame(t(combn(ncsl_bills$matched_from_db, 2))))
-colnames(bill_pairs) <- c("left_doc_id", "right_doc_id") 
-# Also use the reversed combinations
-reverse <- data.frame("left_doc_id" = bill_pairs$right_doc_id,
-                      "right_doc_id" = bill_pairs$left_doc_id)
-bill_pairs <- tbl_df(rbind(bill_pairs, reverse))
-
-# Get 'same-table-indicator'
-## Join with topic tables
-temp <- mutate(ncsl_bills, left_doc_id = matched_from_db, left_table = topic) %>%
-    select(left_doc_id, left_table)
-df <- left_join(bill_pairs, temp, by = "left_doc_id")
-temp <- mutate(ncsl_bills, right_doc_id = matched_from_db, right_table = topic) %>%
-    select(right_doc_id, right_table)
-df <- left_join(df, temp, by = "right_doc_id") %>% 
-    mutate(same_table = ifelse(left_table == right_table, 1, 0))
-
-## Join
-bill_pairs <- left_join(df, alignments, 
-                        by = c("left_doc_id", "right_doc_id"))
-df <- mutate(bill_pairs, alignment_score_NA = ifelse(is.na(alignment_score), 
-                                                        1, 0))
-rm(bill_pairs)
-
-## Put zero for NA alignment score
-df$alignment_score_nona <- ifelse(is.na(df$alignment_score), 0.0001,
-                                          df$alignment_score)
-
-# Numbers for the ncsl section in the paper
-# ==============================================================================
-
-# Frequency of scores higher 50 / 100
-sum(alignments$alignment_score > 50) / nrow(alignments)
-sum(alignments$alignment_score > 100) / nrow(alignments)
-
-# Complete Frequency distribution
-r <- range(alignments$alignment_score)
-thresholds <- exp(seq(log(r[1]), log(r[2]), length.out = 100))
-
-hm <- function(threshold) sum(alignments$alignment_score < threshold) / nrow(alignments)
-cumdist <- sapply(thresholds, hm)
-pdat <- tbl_df(data.frame("Proportion" = cumdist, "Score" = thresholds))
-
-# Load commont theme elements for plots
-source('../plot_theme.R')
-
-ggplot(pdat) + 
-    geom_line(aes(x = Score, y = Proportion), size = 1.2) + 
-    scale_x_log10(breaks = c(1, 10, 100, 1000, 10000)) + 
-    xlab("X") +
-    ylab("P(Score < X)") +
-    plot_theme
-ggsave('../../manuscript/figures/alignment_score_distribution.png', width = p_width, 
-       height = 0.65 * p_width)
-
-
-# Proportion in same table
-sum(df$same_table) / nrow(alignments)
-
-# Store the data
-save(x = df, file = '../../data/ncsl_analysis/ncsl_analysis.RData')
+# Load the data (data preprocessing in `/etl/make_analysis_datasets.R`)
 load('../../data/ncsl_analysis/ncsl_analysis.RData')
-
 
 # Precision recall curve
 # ==============================================================================
 
-r <- range(df$alignment_score_nona, na.rm = TRUE)
+r <- range(df$alignment_score, na.rm = TRUE)
 thresholds <- exp(seq(log(r[1]), log(r[2]), length.out = 100))
 
 score = df$alignment_score_nona
@@ -115,7 +36,7 @@ ggplot(df_1) +
                  alpha = 0.05, color = "white") +
     geom_line(aes(y = value, x = threshold, color = type, linetype = type),
               size = 1.2) + 
-    scale_x_log10() +
+    #scale_x_log10(breaks = c(0, 1, 10, 100)) +
     scale_y_continuous(breaks = c(0, 0.25, 0.5, 0.75, 1, 1.25)) +
     geom_hline(aes(yintercept = 1), linetype = 2, alpha = 0.2) +
     scale_color_manual(values = cbPalette, 
@@ -124,12 +45,11 @@ ggplot(df_1) +
     scale_linetype_manual(values = c(1, 3), 
                        labels = c("Precision", "Recall"),
                        name = "") +
-    xlab("Log Score Threshold") + ylab("Precision / Recall") +
+    xlab("Score Threshold") + ylab("Precision / Recall") +
     plot_theme
 
 ggsave('../../manuscript/figures/ncsl_prec_rec.png', width = p_width, 
        height = 0.65 * p_width)
-
 
 # F1 score
 f1 <- prec_rec[, 1] * prec_rec[ , 2] / (prec_rec[, 1] + prec_rec[ ,2])
@@ -154,5 +74,3 @@ ggplot(df2) +
     theme_bw() + facet_wrap(~group, ncol=1) + 
     ylab("Proportion") + xlab("Alignment Score")
 ggsave('../../manuscript/figures/align_distri_ncsl_tables.png')
-
-
---
+++ etl/make_analysis_datasets.R
@@ -0,0 +1,208 @@
+library(dplyr)
+library(ggplot2)
+
+args <- commandArgs(trailingOnly = TRUE)
+SMALL <- as.logical(args[1])
+
+# Load Alignments and lucene scores
+cat("Loading alignment data...\n")
+
+if(SMALL) {
+    alignments <- tbl_df(read.csv('../data/lid/alignments_1000_b2b_ns.csv', 
+                         header = TRUE, stringsAsFactors = FALSE, nrows = 50000))
+    lucene_scores <- tbl_df(read.csv('../data/lid/lucene_scores_1000.csv',            #still contains same-state dyads
+                         header = TRUE, stringsAsFactors = FALSE, nrows = 50000))
+} else {
+    alignments <- tbl_df(read.csv('../data/lid/alignments_1000_b2b_ns.csv', 
+                         header = TRUE, stringsAsFactors = FALSE))
+    lucene_scores <- tbl_df(read.csv('../data/lid/lucene_scores_1000.csv', 
+                         header = TRUE, stringsAsFactors = FALSE))
+}
+
+# Merge them and discard lucene
+alignments <- left_join(alignments, lucene_scores, by = c("left_doc_id",
+                                                          "right_doc_id"))
+rm(lucene_scores)
+gc()
+
+# ==============================================================================
+# Descriptive statistics for alignments
+# ==============================================================================
+
+# Descriptive stats for alignment dataset
+sink('alignments_descriptives.txt')
+cat(paste0("Number of bill dyads: ", nrow(alignments), '\n'))
+cat(paste0("Mean score: ", mean(alignments$alignment_score), '\n'))
+cat(paste0("Median score: ", median(alignments$alignment_score), '\n'))
+sink()
+
+# Distribution of alignment scores
+r <- range(alignments$alignment_score)
+thresholds <- exp(seq(log(r[1]), log(r[2]), length.out = 100))
+
+hm <- function(threshold) sum(alignments$alignment_score < threshold) / nrow(alignments)
+cumdist <- sapply(thresholds, hm)
+pdat <- tbl_df(data.frame("Proportion" = cumdist, "Score" = thresholds))
+
+# Load commont theme elements for plots
+source('../analysis/plot_theme.R')
+
+p <- ggplot(pdat) + 
+    geom_line(aes(x = Score, y = Proportion), size = 1.2) + 
+    scale_x_log10(breaks = c(1, 10, 100, 1000, 10000)) + 
+    xlab("X") +
+    ylab("P(Score < X)") +
+    plot_theme
+ggsave('../manuscript/figures/alignment_score_distribution.png', width = p_width, 
+       height = 0.65 * p_width)
+
+
+# Relationship of alignment and lucene score
+p <- ggplot(alignments, aes(x = lucene_score, y = alignment_score)) + 
+    stat_binhex(bins = 150) +
+    xlab("Lucene Score") + 
+    ylab("Alignment Score") + 
+    scale_fill_gradient(low = cbPalette[1], high = cbPalette[2], trans = "log",
+                        labels = function (x) round(x, 0)) +
+    guides(fill=guide_legend(title="Count")) +
+    plot_theme
+
+cat('Saving plot...\n')
+ggsave(plot = p, '../manuscript/figures/alignment_lucene.png', 
+       width = p_width, height = 0.65 * p_width)
+
+
+alignments$lucene_score <- NULL
+
+# ==============================================================================
+# Data preprocessing for ideology analysis
+# ==============================================================================
+
+# Load metadata
+cat("Loading metadata...\n")
+meta <- read.csv('../data/lid/bill_metadata.csv', stringsAsFactors = FALSE,
+                 header = TRUE, quote = '"')
+
+## Clean it up
+meta$session <- NULL
+### Make 'None' NA
+meta[meta == 'None'] <- NA
+### Fix column classes
+for(i in grep("date_", names(meta))){
+    var <- sapply(as.character(meta[, i]), substr, 1, 10)
+    meta[, i] <- as.Date(x = var)
+}
+for(col in c("state", "chamber", "bill_type")){
+    meta[, col] <- as.factor(meta[, col])
+}
+meta$sponsor_idology <- as.numeric(meta$sponsor_idology)
+meta$num_sponsors <- as.integer(meta$num_sponsors)
+meta$bill_length <- as.integer(meta$bill_length)
+
+## Make dplyr object
+meta <- tbl_df(meta)
+
+
+## Match alignments with ideology scores and document length
+### Join info on left bill
+cat("Joining datasets...\n")
+temp <- mutate(meta, left_doc_id = unique_id, left_ideology = sponsor_idology,
+               left_length = bill_length) %>%
+    dplyr::select(left_doc_id, left_ideology, left_length)
+df <- left_join(alignments, temp, by = "left_doc_id")
+
+### Join info on right bill
+temp <- mutate(meta, right_doc_id = unique_id, right_ideology = sponsor_idology,
+               right_length = bill_length) %>%
+    dplyr::select(right_doc_id, right_ideology, right_length)
+df <- left_join(df, temp, by = "right_doc_id")
+
+# Calculate ideological distance and combined doc length
+df <- mutate(df, ideology_dist = (left_ideology - right_ideology)^2)
+
+
+# Descriptives for ideology data
+# ******************************************************************************
+
+# Stats for text
+sink('../analysis/ideology/data_desc_stats.txt')
+n <- nrow(df)
+cat(paste0("Preprocessed data from ", n, " bill dyads.\n"))
+df <- filter(df, !is.na(ideology_dist))
+m <- nrow(df)
+cat(paste0((1 - m / n), "% of dyads missing ideological distance.\n"))
+cat(paste0((n - m), " bills don't have ideological distance \n"))
+cat(paste0(m, " valid dyads from ", length(unique(df$left_doc_id)), 
+           " left bills and ", length(unique(df$right_doc_id)), 
+           " right bills remaining.\n"))
+sink()
+
+
+# Save ideology data
+fname <- ifelse(SMALL, "../data/ideology_analysis/ideology_small.RData",
+       "../data/ideology_analysis/ideology.R")
+cat(paste0("Saving to ", fname, "\n"))
+save(df, file = fname)
+
+
+# ==============================================================================
+# Data preprocessing for ncsl analysis
+# ==============================================================================
+
+# Load the ncsl dataset
+ncsl_bills <- tbl_df(read.csv('../data/ncsl/ncsl_data_from_sample_matched.csv',
+                              stringsAsFactors = FALSE, header = TRUE)) %>% 
+    filter(!is.na(matched_from_db)) %>%
+    select(-description, -table) 
+
+# Summary table of tables (for poster)
+group_by(ncsl_bills, topic) %>% summarize(count = n())
+
+# Data Frame of all pairs we have table information of (with the matched db ids)
+bill_pairs <- tbl_df(as.data.frame(t(combn(ncsl_bills$matched_from_db, 2))))
+colnames(bill_pairs) <- c("left_doc_id", "right_doc_id") 
+# Also use the reversed combinations
+reverse <- data.frame("left_doc_id" = bill_pairs$right_doc_id,
+                      "right_doc_id" = bill_pairs$left_doc_id)
+bill_pairs <- tbl_df(rbind(bill_pairs, reverse))
+
+# Get 'same-table-indicator'
+## Join with topic tables
+temp <- mutate(ncsl_bills, left_doc_id = matched_from_db, left_table = topic) %>%
+    select(left_doc_id, left_table)
+df <- left_join(bill_pairs, temp, by = "left_doc_id")
+temp <- mutate(ncsl_bills, right_doc_id = matched_from_db, right_table = topic) %>%
+    select(right_doc_id, right_table)
+df <- left_join(df, temp, by = "right_doc_id") %>% 
+    mutate(same_table = ifelse(left_table == right_table, 1, 0))
+
+## Join
+bill_pairs <- left_join(df, alignments, 
+                        by = c("left_doc_id", "right_doc_id"))
+df <- mutate(bill_pairs, alignment_score_NA = ifelse(is.na(alignment_score), 
+                                                        1, 0))
+rm(bill_pairs)
+
+## Put zero for NA alignment score
+df$alignment_score_nona <- ifelse(is.na(df$alignment_score), 0,
+                                          df$alignment_score)
+
+# Descriptive stats for paper
+# ==============================================================================
+
+# Frequency of scores higher 50 / 100
+sink('../analysis/ncsl/ncsl_data_descriptives.txt')
+cat(paste0('Frequency of scores higher than 50: '), 
+    sum(alignments$alignment_score > 50) / nrow(alignments),
+    '\n')
+cat(paste0('Frequency of scores higher than 50: '), 
+    sum(alignments$alignment_score > 100) / nrow(alignments),
+    '\n')
+cat(paste0('Proportion in same table: '), 
+    sum(df$same_table) / nrow(alignments),
+    '\n')
+
+sink()
+
+# Store the data
+save(x = df, file = '../data/ncsl_analysis/ncsl_analysis.RData')

9abab7cc3a116b11769dc2aa1f92dfe428d3fe6e
flinder <fridolin.linder@gmail.com>
2016-08-23 15:11:35 -0400

    started parallelizing ncsl_alignment calc

--- generate_alignments/ncsl_bill_pairs.py
+++ generate_alignments/ncsl_bill_pairs.py
@@ -21,6 +21,29 @@ import io
 import itertools
 from pprint import pprint
 import numpy as np
+import sys
+import multiprocessing
+
+
+def align_pair(c):
+    '''
+    c: tuple, (left_bill, right_bill)
+    '''
+    alignments = lidy.align_bill_pair(c[0], c[1])
+
+    if alignments is None:
+        l = l_temp.format(c[0], c[1], np.nan)
+        with io.open(outfile_name, 'a', encoding='utf-8') as outfile:
+            outfile.write(l)
+        continue
+
+    for alignment in alignments:
+        pprint(alignment)
+        sys.exit()
+        score = alignment['score']
+        l = l_temp.format(c[0], c[1], score)
+        with io.open(outfile_name, 'a', encoding='utf-8') as outfile:
+            outfile.write(l)
 
 
 logging.basicConfig(level=logging.DEBUG)
@@ -49,14 +72,14 @@ with io.open('../data/ncsl/matched_ncsl_bill_ids.txt') as infile:
 combos = itertools.combinations(bill_ids, 2)
 
 # Uncomment when not picking up bu starting from scratch
-#with io.open(outfile_name, 'w', encoding='utf-8') as outfile:
-#    l = 'left_bill,right_bill,score\n'
-#    outfile.write(l)
+with io.open(outfile_name, 'w', encoding='utf-8') as outfile:
+    l = 'left_bill,right_bill,score\n'
+    outfile.write(l)
 
 l_temp = '{},{},{}\n'
 
 last = ('ma_187th_H4070','wa_2013-2014_SB5419')
-start = False
+start = True
 
 # Loop through all combinations of bills
 for i,c in enumerate(combos):
@@ -65,9 +88,7 @@ for i,c in enumerate(combos):
         if c == last:
             start = True
         continue
-
-    print i
-
+    
     alignments = lidy.align_bill_pair(c[0], c[1])
 
     if alignments is None:
@@ -77,11 +98,9 @@ for i,c in enumerate(combos):
         continue
 
     for alignment in alignments:
+        pprint(alignment)
+        sys.exit()
         score = alignment['score']
         l = l_temp.format(c[0], c[1], score)
         with io.open(outfile_name, 'a', encoding='utf-8') as outfile:
             outfile.write(l)
-
-
-
-

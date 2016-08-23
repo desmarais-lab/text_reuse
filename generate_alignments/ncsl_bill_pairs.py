1da253d2bd4d44b733c557bb7001527687e305f0
Fridolin Linder <fridolin.linder@gmail.com>
2016-08-23 16:12:23 -0400

    parallelized ncsl stuff

--- generate_alignments/ncsl_bill_pairs.py
+++ generate_alignments/ncsl_bill_pairs.py
@@ -1,7 +1,7 @@
 #!/opt/anaconda/bin/python 
 from __future__ import unicode_literals
 import sys 
-sys.path.append('/storage/home/fjl128/bruce_shared/text_reuse/policy_diffusion/lid/')
+sys.path.append('../policy_diffusion/lid/')
 from lid import LID
 from text_alignment import AffineLocalAligner,LocalAligner
 from utils.text_cleaning import clean_document 
@@ -35,68 +35,8 @@ def align_pair(c):
         l = l_temp.format(c[0], c[1], np.nan)
         with io.open(outfile_name, 'a', encoding='utf-8') as outfile:
             outfile.write(l)
-        continue
-
-    for alignment in alignments:
-        pprint(alignment)
-        sys.exit()
-        score = alignment['score']
-        l = l_temp.format(c[0], c[1], score)
-        with io.open(outfile_name, 'a', encoding='utf-8') as outfile:
-            outfile.write(l)
-
-
-logging.basicConfig(level=logging.DEBUG)
-logging.getLogger('elasticsearch').setLevel(logging.ERROR)
-logging.getLogger('urllib3').setLevel(logging.ERROR)
-logging.getLogger('json').setLevel(logging.ERROR)
-
-outfile_name = '../data/alignments_new/ncsl_pair_alignments.csv'
-         
-# Initialize aligner
-aligner = AffineLocalAligner(match_score=4, mismatch_score=-1, gap_start=-3, 
-                             gap_extend = -1.5)
-
-
-# Initialize LID
-ES_IP = "54.244.236.175"
-lidy = LID(query_results_limit=1000, elastic_host=ES_IP, 
-           lucene_score_threshold=0, aligner=aligner)
-
-
-# Load list of bills
-with io.open('../data/ncsl/matched_ncsl_bill_ids.txt') as infile:
-    bill_ids = [s.strip('\n') for s in infile.readlines()]
-
-
-combos = itertools.combinations(bill_ids, 2)
-
-# Uncomment when not picking up bu starting from scratch
-with io.open(outfile_name, 'w', encoding='utf-8') as outfile:
-    l = 'left_bill,right_bill,score\n'
-    outfile.write(l)
-
-l_temp = '{},{},{}\n'
-
-last = ('ma_187th_H4070','wa_2013-2014_SB5419')
-start = True
-
-# Loop through all combinations of bills
-for i,c in enumerate(combos):
-
-    if not start: 
-        if c == last:
-            start = True
-        continue
+        return None
     
-    alignments = lidy.align_bill_pair(c[0], c[1])
-
-    if alignments is None:
-        l = l_temp.format(c[0], c[1], np.nan)
-        with io.open(outfile_name, 'a', encoding='utf-8') as outfile:
-            outfile.write(l)
-        continue
-
     for alignment in alignments:
         pprint(alignment)
         sys.exit()
@@ -104,3 +44,53 @@ for i,c in enumerate(combos):
         l = l_temp.format(c[0], c[1], score)
         with io.open(outfile_name, 'a', encoding='utf-8') as outfile:
             outfile.write(l)
+        return None
+
+
+if __name__ == '__main__':
+
+    logging.basicConfig(level=logging.DEBUG)
+    logging.getLogger('elasticsearch').setLevel(logging.ERROR)
+    logging.getLogger('urllib3').setLevel(logging.ERROR)
+    logging.getLogger('json').setLevel(logging.ERROR)
+
+    outfile_name = '../data/alignments_new/ncsl_pair_alignments.csv'
+             
+    # Initialize aligner
+    aligner = AffineLocalAligner(match_score=4, mismatch_score=-1, gap_start=-3, 
+                                 gap_extend = -1.5)
+
+    # Initialize LID
+    ES_IP = "54.244.236.175"
+    lidy = LID(query_results_limit=1000, elastic_host=ES_IP, 
+               lucene_score_threshold=0, aligner=aligner)
+
+
+    # Load list of bills
+    with io.open('../data/ncsl/matched_ncsl_bill_ids.txt') as infile:
+        bill_ids = [s.strip('\n') for s in infile.readlines()]
+
+
+    c = itertools.combinations(bill_ids, 2)
+    combos = [x for x in c]
+    
+    # Uncomment when not picking up bu starting from scratch
+    with io.open(outfile_name, 'w', encoding='utf-8') as outfile:
+        l = 'left_bill,right_bill,score\n'
+        outfile.write(l)
+
+    l_temp = '{},{},{}\n'
+
+    #last = ('ma_187th_H4070','wa_2013-2014_SB5419')
+    #start = True
+
+    pool = multiprocessing.Pool(processes=4)
+    #pool.map(align_pair, combos)
+    #pool.close()
+    #pool.join()
+
+    def t(x):
+        return x + 1
+
+    l = [1,2,3,4]
+    results = pool.map_async(t, l).get(9999999)

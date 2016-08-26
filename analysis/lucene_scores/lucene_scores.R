f66584055741e1835744dbd725621833c23a9ba6
Fridolin Linder <fridolin.linder@gmail.com>
2016-08-26 09:55:02 -0400

    small update to lucene analysis

--- analysis/lucene_scores/lucene_scores.R
+++ analysis/lucene_scores/lucene_scores.R
@@ -4,5 +4,7 @@ library(ggplot2)
 
 load('../../data/lucene_analysis/lucene_analysis.RData')
 
+samp <- alignments[sample(c(1:nrow(alignments)), 100000), ]
 
-cor(alignments$alignment_score, alignments$lucene_score)
+
+ggplot(samp) + geom_point(aes(x = lucene_score, y = alignment_score))

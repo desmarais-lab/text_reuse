44720628917beb152e6c1d466db384d9b793c63e
Fridolin Linder <fridolin.linder@gmail.com>
2016-09-21 16:33:39 -0400

    no quantile lines in ideology plot

--- analysis/ideology/ideology_plot.R
+++ analysis/ideology/ideology_plot.R
@@ -48,18 +48,3 @@ p <- ggplot(df, aes(x = ideology_dist, y = alignment_score)) +
 cat('Saving plot...\n')
 ggsave(plot = p, '../../manuscript/figures/ideology_plot_log.png', 
        width = p_width, height = 0.65 * p_width)
-
-p <- ggplot(df, aes(x = ideology_dist, y = alignment_score)) + 
-    stat_binhex(bins = 300) + 
-    stat_quantile(quantiles = quantiles, color = cbPalette[6], 
-                  method.args = list("method" = "pfn")) + 
-    xlab("Ideological Distance") + 
-    ylab("Alignment Score") + 
-    scale_fill_gradient(low = cbPalette[1], high = cbPalette[2], trans = "log",
-                        labels = function (x) round(x, 0)) +
-    guides(fill=guide_legend(title="Count")) +
-    plot_theme
-
-cat('Saving plot...\n')
-ggsave(plot = p, '../../manuscript/figures/ideology_quantile_plot.png', 
-       width = p_width, height = 0.65 * p_width)

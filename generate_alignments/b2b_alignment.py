337573631f0d3bc3dd284aa87de4dbd61c6ceb68
Fridolin Linder <fridolin.linder@gmail.com>
2017-02-09 16:57:13 -0500

    rewrote alignment generation from scratch

---
+++ generate_alignments/b2b_alignment.py
@@ -0,0 +1,187 @@
+from elasticsearch import Elasticsearch as ES
+from text_cleaning import clean_document
+from pprint import pprint
+from time import time
+from urllib3.exceptions import NewConnectionError
+from socket import gaierror
+from local_aligner import align, TimeOutError
+import sys
+import csv
+import os
+
+
+def similar_doc_query(es_connection, text, state_id, num_results):
+    """
+     Query db for similar documents within documents of not the same
+     state
+    """
+    
+    # Elastic search query:
+    query = {
+        "query": {
+            "bool": {
+                "must": [
+                    {
+                        "more_like_this": {
+                            "fields": ["bill_document_last.shingles"],
+                            "like_text": text,
+                            "max_query_terms": 25,
+                            "min_term_freq": 1,
+                            "min_doc_freq": 2,
+                            "minimum_should_match": 1
+                        }
+                    },
+                    {
+                        "bool": { "must_not": { "match": {
+                                    "state": state_id
+                                }
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    }
+
+    results = es_connection.search(index="state_bills", body=query,
+                                   fields=["state", "bill_document_last"], 
+                                   size=num_results)
+    max_score = results['hits']['max_score']
+    results = results['hits']['hits']
+    result_docs = []
+    for res in results:
+        doc = {}
+        for f in res['fields']:
+            doc[f] = res['fields'][f][0]
+        doc['score'] = res['_score']
+        doc['id'] = res['_id']
+        result_docs.append(doc)
+
+    return result_docs, max_score
+
+def write_outputs(alignments, bill_id, status, time, n_bills, n_success, 
+                  output_dir):
+    
+    alignment_file = os.path.join(output_dir, 'alignments/', 
+                                  bill_id + '_alignments.csv')
+    status_file = os.path.join(output_dir, 'bill_status/', 
+                               bill_id + '_status.csv')
+
+    with open(alignment_file, 'w') as af, open(status_file, 'w') as sf:
+
+        swriter = csv.writer(sf, delimiter=',', quotechar='"', 
+                             quoting=csv.QUOTE_MINIMAL)
+
+        swriter.writerow([bill_id, status, round(time, 4), n_bills, n_success])
+        
+        if len(alignments) > 0:
+            awriter = csv.writer(af, delimiter=',', quotechar='"', 
+                                 quoting=csv.QUOTE_MINIMAL)
+     
+            for a in alignments:
+                awriter.writerow(a)
+
+
+class NoConnectionError(Exception):
+    pass
+
+class NoTextError(Exception):
+    pass
+
+class NoBillError(Exception):
+    pass
+
+if __name__ == "__main__":
+
+    # =========================================================================
+    # Config
+    # =========================================================================
+    BILL_ID = sys.argv[1]
+    ES_IP = ["http://elasticsearch.dssg.io:9200/"]
+    
+    N_RIGHT_BILLS = 500
+    MATCH_SCORE = 3
+    MISMATCH_SCORE = -2
+    GAP_SCORE = -3
+    #OUTPUT_DIR = "/storage/group/bbd5087_collab/text_reuse/data/aligner_output"
+    OUTPUT_DIR = "../data/aligner_output"
+    # =========================================================================
+    
+    alignments = []
+    status = "successfull"
+    start_time = time()
+    n_right_bills = None
+    n_success = 0
+
+    try:
+        # Establish elastic search connection
+        es = ES(ES_IP, timeout = 100)
+
+        if not es.ping():
+            raise NoConnectionError()
+
+        # Get text of the left bill
+        query_doc = es.get_source(index="state_bills", id=BILL_ID, doc_type="_all")
+
+        if query_doc is None:
+            raise NoBillError()
+
+
+        # Get most similar right bills
+        if query_doc["bill_document_last"] is not None:
+            query_text = query_doc["bill_document_last"]
+        elif query_doc["bill_document_first"] is not None:
+            query_text = query_doc["bill_document_first"]
+        else:
+            raise NoTextError()
+
+        query_text = clean_document(query_text, state_id=BILL_ID[:2])
+        res, max_score = similar_doc_query(es_connection=es, text=query_text, 
+                                           state_id=BILL_ID[:2], 
+                                           num_results=N_RIGHT_BILLS)
+
+        n_right_bills = len(res)
+                
+        # Align them
+        for right_bill in res:
+
+            right_text = clean_document(right_bill["bill_document_last"],
+                                        state_id=right_bill["state"])
+            s = time()
+            try:
+                alignment = align(left=query_text, right=right_text, 
+                                  match=MATCH_SCORE, gap=GAP_SCORE,
+                                  mismatch=MISMATCH_SCORE)
+            except TimeOutError:
+                alignment = [None, None, None] 
+            at = time() - s
+
+            alignment.insert(0, right_bill["id"])
+            alignment.insert(0, BILL_ID)
+            alignment.extend([round(right_bill["score"], 4), 
+                              round(max_score, 4), round(at, 4)])
+
+            alignments.append(alignment)
+            n_success += 1
+
+    # Handle exceptions
+    except NoConnectionError:
+        status = "connection_error"
+
+    except NoTextError:
+        status = "no_text"
+
+    except NoBillError:
+        status = "no_bill"
+
+    except KeyboardInterrupt as e:
+        raise e
+
+    except Exception as e:
+        status = "other_error"
+        raise e
+
+    finally:
+        elapsed_time = time() - start_time 
+        write_outputs(alignments, BILL_ID, status, elapsed_time, n_right_bills,
+                      n_success, OUTPUT_DIR)
---
+++ generate_alignments/text_cleaning.py
@@ -0,0 +1,194 @@
+'''
+Clean text in ElasticSearch
+'''
+
+import re
+from elasticsearch import Elasticsearch
+
+#custom modules
+#from database import ElasticConnection
+
+def clean_text(text, lower = True):
+    '''
+    variables:
+        text: string corresponding to text of bill
+        bill_name: string corresponding to bill_id
+
+    returns:
+        string that is cleaned up text 
+    decription:
+        clean text 
+    '''
+    #make text lowercase
+    if lower == True:
+        text = text.lower()
+    
+    #parse by line
+    text_list =  text.splitlines()
+
+    #replace funky symbols and multipe new lines
+    ntext_list = []
+    for line in text_list:
+        line = line.replace(u'\xa0', u' ')
+        line = line.replace(u'>>', u' ')
+        line = line.replace(u'\xa7', u' ')
+        line = line.replace(u'\xe2', u' ')
+        line = line.replace(u'\u201c', u' ')
+        line = line.replace(u'\u201d', u' ')
+        line = line.replace(u'\xbb', u' ')
+        line = line.replace(u'\xa9', u' ')
+        line = line.replace(u'{ font-family: courier, arial, sans-serif; font-size: 10pt; } table { empty-cells:show; }', u' ')
+        line = re.sub( '\s+', ' ', line)
+        line = re.sub( '"', '', line)
+        ntext_list.append(line)
+
+    return "\n".join(ntext_list)
+ 
+ 
+
+
+   
+
+def split_to_sections(cleantext,state):
+    '''
+    variables:
+        cleantext: clean version of text of bill
+        state: abbreviation of state ID
+
+    returns:
+        list of bill sections
+    decription:
+        splits bill text into sections
+    '''
+    if state == 'ak':
+        chunked_list = cleantext.split("\n*")
+    elif state in ('al','ar','mt','or','ri'):
+        chunked_list = cleantext.split('\nsection')
+    elif state in ('nm','tx'):
+        chunked_list = cleantext.split('\n section')
+    elif state in ('az','ia','nv', 'wa', 'vt'):
+        chunked_list = cleantext.split('\nsec.')
+    elif state in ('me', 'mi'):
+        chunked_list = cleantext.split('\n sec.')
+    elif state == 'co':
+        chunked_list = re.split('[[0-9][0-9]\.section|[0-9]\.section', cleantext)
+    elif state in ('de','fl','tn'):
+        chunked_list = re.split('section\s[0-9][0-9]\.|section\s[0-9]\.', cleantext)
+    elif state == 'ga':
+        cleantext = re.sub('[0-9][0-9]\\n|[0-9]\\n', ' ', cleantext)
+        chunked_list = re.split('\\nsection\s[0-9][0-9]|\\nsection\s[0-9]', cleantext)
+    elif state in ('hi','sd','in'):
+        chunked_list = re.split('\\n\ssection\s[0-9][0-9]\.|\\n\ssection\s[0-9]', cleantext)
+    elif state == 'pa':
+        chunked_list = re.split('section\s[0-9][0-9]\.|section\s[0-9]\.', cleantext) 
+    elif state in ('id', 'la', 'md', 'nd'):
+        chunked_list = re.split('\\nsection\s[0-9][0-9]\.|\\nsection\s[0-9]\.', cleantext)
+    elif state == 'il':
+        cleantext = re.sub('\\n\s[0-9][0-9]|\\n\s[0-9]', ' ', cleantext)
+        chunked_list = re.split('\\n\s\ssection\s', cleantext)
+    elif state == 'sc':
+        chunked_list = cleantext.split('\n \n')
+    elif state == 'ks':
+        chunked_list = re.split('\\nsection\s|sec\.', cleantext)
+    elif state in ('ne', 'mn'):
+        chunked_list = re.split('\ssection\s[0-9]\.|\ssec.\s[0-9][0-9]\.|\ssec.\s[0-9]\.', cleantext)
+    elif state == 'ky':
+        chunked_list = cleantext.split('\n\n\n section .')
+    elif state == 'ms':
+        chunked_list = cleantext.split('\n\n\n section ')
+    elif state in ('ma', 'nc', 'oh','ut'):
+        chunked_list = re.split('\ssection\s[0-9][0-9]\.|\ssection\s[0-9]\.', cleantext)
+    elif state == 'mo':
+        chunked_list = re.split('\\n\s[0-9][0-9]\.\s|\\n\s[0-9]\.\s', cleantext)
+    elif state == 'nh':
+        chunked_list = re.split('\n\n[0-9][0-9]\s|\n\n[0-9]\s', cleantext)
+    elif state == 'nj':
+        chunked_list = re.split('\\n\\n\s[0-9][0-9]\.\s|\\n\\n\s[0-9]\.\s', cleantext)
+    elif state == 'ny':
+        chunked_list = re.split('\ssection\s[0-9]\.|\.\ss\s[0-9]\.', cleantext)
+    elif state == 'ok':
+        chunked_list = re.split('\nsection\s\.\s', cleantext)
+    elif state == 'va':
+        chunked_list = re.split('(([A-Z])|[0-9][0-9])\.\s|(([A-Z])|[0-9])\.\s', cleantext)
+    elif state == 'wi':
+        chunked_list = re.split('\\n[0-9][0-9]section\s\\n|\\n[0-9]section\s\\n', cleantext)
+    elif state == 'wv':
+        chunked_list = re.split('\n\s\([a-z]\)\s', cleantext)
+    elif state == 'wy':
+        chunked_list = re.split('\ssection\s[0-9][0-9]\.|\ssection\s[0-9]\.', cleantext)
+    elif state == 'ca':
+        chunked_list = re.split('section\s[0-9]\.|sec.\s[0-9][0-9]\.|sec.\s[0-9]\.', cleantext)
+    elif state == None:
+        chunked_list = cleantext.split("\n")
+    else:
+        chunked_list = cleantext.split("\n")
+
+    return chunked_list
+
+#Delete empty sections (run before deleting numbers in lines)
+def delete_empty_sections(chunked_list):
+    '''
+    decription: deletes empty elements in bills
+    '''
+    return [x for x in chunked_list if x is not None and len(x)>2] 
+
+#Need to delete number lines for: OR, OK, NE, PA (run before deleting lines) 
+def delete_numbers_in_lines (chunked_list):
+    '''
+    decription:
+        cleans pdf extractor errors where number of lines were included in text
+    '''
+    re_string = '\\n\s[0-9][0-9]|\\n[0-9][0-9]|\\n[0-9]|\\n\s[0-9]'
+    chunked_list = [re.sub(re_string,'',t) for t in chunked_list]
+    return chunked_list
+
+
+
+#Delete multiple new lines for each section
+def delete_lines (chunked_list):
+    '''
+    description: deletes multiple lines and spaces for each section
+    '''
+    chunked_list = [re.sub( '\s+', ' ', x) for x in chunked_list]
+    return chunked_list
+        
+
+
+def clean_document(doc_text, state_id):
+    """text -- document text"""
+    
+    doc_text = clean_text(doc_text)
+    doc_text_sections = split_to_sections(doc_text, state_id)
+    doc_text_sections = delete_empty_sections(doc_text_sections)
+    if state_id in ['or','ok','ne','pa']:
+        doc_text_sections = delete_numbers_in_lines(doc_text_sections)
+    doc_text_sections = delete_lines(doc_text_sections)
+
+    return " ".join(doc_text_sections)
+
+#delete boiler plate present in all alec exposed bills after "effective date"
+def delete_boiler_plate_alec_exposed (chunked_list):
+    chunked_list = [re.sub('({effective date).*$', ' ', x) for x in chunked_list]
+    chunked_list = chunked_list[1:]
+    return chunked_list
+
+#good example is test_clean_text_for_alignment('va')
+
+def test_clean_text(state):
+   es = Elasticsearch(['54.203.12.145:9200', '54.203.12.145:9200'], timeout=300)
+   match = es.search(index="state_bills", body={"query": {"match": {'state': state}}})
+   state_text = match['hits']['hits'][3]['_source']['bill_document_first']
+   cleaned_doc = clean_document(state_text,doc_type = "state_bill",state_id = "mi",split_to_section = False)
+   return cleaned_doc
+
+def main():
+    #Get data from elasticsearch to test
+    
+    print(test_clean_text("mi"))
+
+if __name__ == "__main__":
+    main()
+
+
+
+

303a0e8d1e1e83c49859021016569aff2e5f48ed
flinder <fridolin.linder@gmail.com>
2017-02-10 13:49:00 -0500

    script to test cluster health

---
+++ generate_alignments/cluster_health.py
@@ -0,0 +1,45 @@
+
+
+
+
+from elasticsearch import Elasticsearch
+from pprint import pprint
+
+
+es = Elasticsearch('http://elasticsearch.dssg.io:9200/', timeout=1000)
+pprint(es.cluster.health())
+print(es.ping())
+
+doc = es.get_source(index="state_bills", id='ky_2013RS_HB262', doc_type="_all")
+pprint(doc.keys())
+   
+query = {
+    "query": {
+        "bool": {
+            "must": [
+                {
+                    "more_like_this": {
+                        "fields": ["bill_document_last.shingles"],
+                        "like_text": "this is some example text that serves th",
+                        "max_query_terms": 25,
+                        "min_term_freq": 1,
+                        "min_doc_freq": 2,
+                        "minimum_should_match": 1
+                    }
+                },
+                {
+                    "bool": { "must_not": { "match": {
+                                "state": "ca"
+                            }
+                        }
+                    }
+                }
+            ]
+        }
+    }
+}
+
+
+res = es.search(index="state_bills", body=query, size=500)
+print(res.keys())
+print(len(res['hits']['hits']))

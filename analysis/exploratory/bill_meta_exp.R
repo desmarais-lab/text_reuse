82185fbd6a49c24a349b0772bea7c1f4d1103fdd
Fridolin Linder <fridolin.linder@gmail.com>
2017-03-21 16:22:46 -0400

    updated bill_meta_exp.R with new data (less bills with text)

--- analysis/exploratory/bill_meta_exp.R
+++ analysis/exploratory/bill_meta_exp.R
@@ -3,25 +3,30 @@ library(ggplot2)
 
 source('../plot_theme.R')
 
-df <- tbl_df(read.csv(file = '../../data/lid/bill_metadata.csv', header = TRUE,
+# Load original sunlight bill metadata
+full_meta <- tbl_df(read.csv(file = '../../data/bill_metadata.csv', header = TRUE,
                       stringsAsFactors = FALSE))
- 
-# Get bill prefixes and introduction years
-# f <- function(x) return(x[length(x)])
-# df <- mutate(df, id = sapply(strsplit(unique_id, "_"), f),
-#              prefix = gsub("[[:digit:]-]", "", id),
-#              date_introduced = as.Date(df$date_introduced),
-#              year_introduced = format(date_introduced, "%Y"),
-#              bill_length = as.integer(bill_length)
-#              )
-#     
-# prefixes <- group_by(df, state, prefix) %>% summarize(count = n())
-# 
-# coverage <- group_by(df, state, year_introduced) %>% summarize(count = n())
-# write.csv(coverage, '../../data/lid/nbills_by_state.csv', row.names = FALSE)
-coverage <- tbl_df(read.csv('../../data/lid/nbills_by_state.csv', 
-                            stringsAsFactors = FALSE))
 
+# Load the list of bills in the database (only bills with text available)
+bill_list <- tbl_df(read.csv(file = '../../data/bill_ids.txt', 
+                             stringsAsFactors = FALSE, header = FALSE))
+colnames(bill_list) <- c("unique_id")
+
+df <- left_join(bill_list, full_meta)
+
+# Get bill prefixes and introduction years
+f <- function(x) return(x[length(x)])
+df <- mutate(df, id = sapply(strsplit(unique_id, "_"), f),
+              prefix = gsub("[[:digit:]-]", "", id),
+              date_introduced = as.Date(df$date_introduced),
+              year_introduced = format(date_introduced, "%Y"),
+              bill_length = as.integer(bill_length)
+              )
+prefixes <- group_by(df, state, prefix) %>% summarize(count = n())
+coverage <- group_by(df, state, year_introduced) %>% summarize(count = n())
+#write.csv(coverage, '../../data/lid/nbills_by_state.csv', row.names = FALSE)
+#coverage <- tbl_df(read.csv('../../data/lid/nbills_by_state.csv', 
+#                            stringsAsFactors = FALSE))
 
 # State distribution bill counts
 state_desc <- mutate(df, date_introduced = as.Date(df$date_introduced)) %>%
@@ -36,9 +41,8 @@ ggplot(state_desc, aes(x=year_introduced, y=state)) +
     ylab("State") + xlab("Year") + 
     guides(size=guide_legend(title="Count")) +
     plot_theme
-ggsave('../../4344753rddtnd/figures/year_count_by_state.png', width = p_width,
+ggsave('../../manuscript/figures/year_count_by_state.png', width = p_width,
        height = 0.7*p_width)
-
 
 ## Amount of text by state
 # amount <- mutate(df, date_introduced = as.Date(df$date_introduced)) %>%
